12e10be86dc8fc4453be7a5d2e9a1d0ad63ff411
ibrahim abdulhemit <93004625+abdulhemit@users.noreply.github.com>
2022-12-16 12:20:20 +0300

    LoginPageUI was finish.

--- app/src/main/java/com/example/loginpageui/MainActivity.kt
+++ app/src/main/java/com/example/loginpageui/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.loginpageui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.loginpageui.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.singUp.setOnClickListener {
+            binding.singUp.background = resources.getDrawable(R.drawable.switch_trcks,null)
+            binding.singUp.setTextColor(resources.getColor(R.color.textColor))
+            binding.logIn.background = null
+            binding.logIn.setTextColor(resources.getColor(R.color.pickColor))
+            binding.SingUpLayout.visibility = View.VISIBLE
+            binding.loginLayout.visibility = View.GONE
+        }
+        binding.logIn.setOnClickListener {
+            binding.logIn.background = resources.getDrawable(R.drawable.switch_trcks,null)
+            binding.logIn.setTextColor(resources.getColor(R.color.textColor))
+            binding.singUp.background = null
+            binding.singUp.setTextColor(resources.getColor(R.color.pickColor))
+            binding.SingUpLayout.visibility = View.GONE
+            binding.loginLayout.visibility = View.VISIBLE
+        }
+        binding.singIn.setOnClickListener {
+            startActivity(Intent(this,newActivity::class.java))
+        }
+
     }
 }
